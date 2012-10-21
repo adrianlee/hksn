62495952aa8d366dfaa6d602ebe5cec506524353
Adrian Lee <jun.irok@gmail.com>
2012-10-21 01:39:01 -0400

    Hook up tumblr. Added sponsor page.

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ var express = require('express'),
     // Third Party
     hbs = require('hbs');
 
+var Tumblr = require('tumblr').Tumblr;
+var blog = new Tumblr('mcgillhksn.tumblr.com', 'kJChG5pOCSiCOXbjefjZFKk0mDWL6X25On0t6hi0uN4JNzqQgo');
+
+var FB = require('fb');
+FB.setAccessToken('AAACEdEose0cBAGPHsjqEOjjFNeEy5y7GeEa8CrXZBUwYWGoDYsU3uBqrSSZBbWQZBZCPKcRQLzgUpmHoUOaEbCGnPBNuZBCmsZAzokfpw4CwZDZD');
+
+var moment = require('moment');
+
 var app = express();
 
 ////////////////////////////////////////////////
@@ -44,45 +52,87 @@ hbs.registerHelper('extend', function(name, context) {
 hbs.registerHelper('block', function(name) {
     var val = (blocks[name] || []).join('\n');
 
-    // clear the block
     blocks[name] = [];
     return val;
+});
+
+hbs.registerHelper('unescape', function (post) {
+  var html;
+  html = unescape(post);
+  return html;
+});
+
+hbs.registerHelper('post-date-tag', function (post) {
+  var date,
+      string;
+
+  date = moment.unix(post.timestamp).fromNow();
+  string = date + " - " + post.tags;
+
+  return string;
 });
 
 ////////////////////////////////////////////////
 // Router
 ////////////////////////////////////////////////
 app.get('/', function(req, res) {
-  res.render('index', { title: 'HKSN' });
-});
-
-app.get('/page', function(req, res) {
-  res.render('page', { title: 'HKSN' });
-});
-
-app.get('/events', function(req, res) {
-  res.render('events', { title: 'HKSN' });
-});
-
-app.get('/academics', function(req, res) {
-  res.render('academics', { title: 'HKSN' });
-});
-
-app.get('/events', function(req, res) {
-
-  var Tumblr = require('tumblr').Tumblr;
-  var blog = new Tumblr('mcgillhksn.tumblr.com', 'kJChG5pOCSiCOXbjefjZFKk0mDWL6X25On0t6hi0uN4JNzqQgo');
-
-  console.log(blog);
-
-  blog.text({limit: 2}, function(error, response) {
+  blog.posts({limit: 3}, function(error, response) {
     if (error) {
       throw new Error(error);
     }
-
-    console.log(response.posts);
-    res.render('events', { title: 'HKSN', blog: response.posts });
+    res.render('index', { title: 'HKSN', posts: response.posts });
   });
+});
+
+app.get('/page', function(req, res) {
+  FB.api('376965089046024/attending', { fields: ['id', 'name'] }, function (fbres) {
+    if(!fbres || fbres.error) {
+      console.log(fbres.error);
+      return;
+    }
+    console.log(fbres);
+    res.render('page', { title: 'HKSN' });
+  });
+});
+
+app.get('/academics', function(req, res) {
+  blog.posts({tag: "academics"}, function(error, response) {
+    if (error) {
+      throw new Error(error);
+    }
+    res.render('academics', { title: 'HKSN', posts: response.posts });
+  });
+});
+
+app.get('/events', function(req, res) {
+  blog.posts({tag: "events"}, function(error, response) {
+    if (error) {
+      throw new Error(error);
+    }
+    res.render('events', { title: 'HKSN', posts: response.posts });
+  });
+});
+
+app.get('/food', function(req, res) {
+  blog.posts({tag: "food"}, function(error, response) {
+    if (error) {
+      throw new Error(error);
+    }
+    res.render('food', { title: 'HKSN', posts: response.posts });
+  });
+});
+
+app.get('/news', function(req, res) {
+  blog.posts({tag: "news"}, function(error, response) {
+    if (error) {
+      throw new Error(error);
+    }
+    res.render('news', { title: 'HKSN', posts: response.posts });
+  });
+});
+
+app.get('/sponsors', function(req, res) {
+  res.render('sponsors', { title: 'HKSN' });
 });
 
 ////////////////////////////////////////////////
